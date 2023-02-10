82c0901ea6989b9e382b7e9a3e6125a083972c6f
haydonturner <0676627@schoolsnet.act.edu.au>
2023-02-10 14:40:48 +1100

    updated hello

--- template.php
+++ template.php
@@ -1,9 +1,9 @@
 <?php
 
-function footer(): string
+function footer():string
 {
-
+    date_default_timezone_set('Australia/Canberra');
     $filename = basename($_SERVER["SCRIPT_FILENAME"]);
-    $footer = "this page was last modified: " . date("F d Y H:i:s.", filetime($filename));
+    $footer = "This page was last modified: " . date("F d Y H:i:s.", filemtime($filename));
     return $footer;
 }
