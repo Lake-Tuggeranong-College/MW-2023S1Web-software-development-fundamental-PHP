71e097bd71f3499507644a1e98a320309d2e5d59
haydonturner <0676627@schoolsnet.act.edu.au>
2023-02-10 14:17:53 +1100

    updated hello

--- template.php
+++ template.php
@@ -1,5 +1,9 @@
 <?php
-footer()
-$filename = basename($_SERVER["SCRIPT_FILENAME"]);
-$footer = "this page was last modified: " . date( format: "F d Y H:i:s.", filetime($filename));
-return $footer;
+
+function footer(): string
+{
+
+    $filename = basename($_SERVER["SCRIPT_FILENAME"]);
+    $footer = "this page was last modified: " . date(format: "F d Y H:i:s.", filetime($filename));
+    return $footer;
+}
