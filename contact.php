e9145b5a1e5eeabf0d40f9b61334393bb30a0d5e
haydonturner <0676627@schoolsnet.act.edu.au>
2023-02-15 11:25:09 +1100

    contact

--- contact.php
+++ contact.php
@@ -10,34 +10,30 @@
 <body>
 <h1>Contact Us</h1>
 <div class="container-fluid">
-    <h1 class="text-primary">Please Send us a Message</h1>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <form action="contact.php" method="post">
         <div class="mb-3">
-            <label for="contactEmail" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="contactEmail" name="contactEmail"
-                   placeholder="name@example.com">
+            <label for="inputEmail" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="inputEmail" name="inputEmail" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
-            <label for="contactMessage" class="form-label">Message</label>
-            <textarea class="form-control" id="contactMessage" name="contactMessage" rows="3"></textarea>
+            <label for="inputMessage" class="form-label">Message</label>
+            <input type="text" class="form-control" id="inputMessage" name="inputMessage">
         </div>
-        <button type="submit" name="formSubmit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
 
-
 <?php
-if (isset($_POST['formSubmit']))
-{
-    $userEmail = sanitiseData($_POST['contactEmail']);
-    $userMessage = sanitiseData($_POST['contactMessage']);
-
-    echo $userEmail;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $emailAddress = $_POST['inputEmail'];
+    $messageSubmitted = $_POST['inputMessage'];
+    echo $emailAddress;
     echo "<p>";
-    echo $userMessage;
+    echo $messageSubmitted;
 }
-
 ?>
+
 ?>
 <?php echo footer() ?>
 </body>
